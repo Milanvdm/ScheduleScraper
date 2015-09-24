23f7af425ad97b276a5c185cfe1e1d9e5ec28b5a
Milan van der Meer <milanvdm@hotmail.com>
2015-09-25 01:08:35 +0200

    Works!

--- src/cmd/Main.java
+++ src/cmd/Main.java
@@ -3,6 +3,7 @@ package cmd;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.text.ParseException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -24,10 +25,35 @@ public class Main {
 			// split line into arguments
 			args = line.split(" "); 
 
-			cli.parse(args);
+			if(args.length > 1) {
+				String[] newArgs = new String[2];
+
+				newArgs[0] = args[0];
+
+				String[] strings = Arrays.copyOfRange(args, 1, args.length);
+				StringBuilder builder = new StringBuilder();
+
+				for (String string : strings) {
+					if (builder.length() > 0) {
+						builder.append(" ");
+					}
+					builder.append(string);
+				}
+
+				String string = builder.toString();
+
+				newArgs[1] = string;
+
+				System.out.println(string);
+				
+				cli.parse(newArgs);
+			}
+			else {
+				cli.parse(args);
+			}
 
 		}
-		
+
 		sc.close();
 
 
